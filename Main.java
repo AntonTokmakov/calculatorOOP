901046b64c8c097460dc739ef763161a947fec1c
Anton <antontokmakov757@gmail.com>
2024-01-14 15:16:33 +0700

    Example work

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 package examples.calculator;
 
-// Создать логирование операций, для класса Calculator. Класс Calculator не должен изменится.
 public class Main {
     public static void main(String[] args) {
         ICalculableFactory calculableFactory = new CalculableFactory();
